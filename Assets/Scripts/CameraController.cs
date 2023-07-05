130de33034322eac865376186ada8e670a7121c0
SamSuum <121036924+SamSuum@users.noreply.github.com>
2023-07-05 11:16:18 +0100

    fix

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    
-    
+
+    //fields
+    public Transform followTarget;
     [SerializeField] float sensitivity = 1;
-
+    //properties
     float mouseY, mouseX;
-
-  
-
+    
     // Update is called once per frame
     void Update()
     {
-        
 
-        mouseY += Input.GetAxis("Mouse Y")*sensitivity;
-        mouseX += Input.GetAxis("Mouse X")*sensitivity;
+       // Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
 
-        transform.localRotation = Quaternion.Euler(-mouseY, mouseX, 0);
-        
+        mouseX += Input.GetAxis("Mouse Y")*sensitivity; 
+        mouseY += Input.GetAxis("Mouse X")*sensitivity;
+
+
+
+        if ( mouseX < 45 && mouseX > -25)
+        {
+            followTarget.transform.localRotation = Quaternion.Euler(-mouseX,0, 0);                   
+            transform.localRotation = Quaternion.Euler(0, mouseY, 0);            
+        }
+
     }
    
 }
--- Assets/Scripts/Player_Animations.cs
+++ Assets/Scripts/Player_Animations.cs
@@ -6,13 +6,15 @@ public class Player_Animations : MonoBehaviour
 {
     //references
     private Animator anim;
-    
-    //Fields
+
+    //Fields    
+    [SerializeField] float acceleration = 2.0f;
+    [SerializeField] float deceleration = 2.0f;
+
+    //properties
     float velocityZ = 0.0f;
     float velocityX = 0.0f;
-    [SerializeField] float maxVelocity;
-    [SerializeField] float acceleration = 0.5f;
-    [SerializeField] float deceleration = 3.0f;
+    float maxVelocity;
 
 
     // Start is called before the first frame update
@@ -23,55 +25,87 @@ public class Player_Animations : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        #region Attack
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            StartCoroutine(AnimAttack());
-        }
-        else anim.SetBool("Slash", false);
-        #endregion
+         
+         #region Attack
 
+         if (Input.GetKeyDown(KeyCode.Mouse0))
+         {
+             StartCoroutine(AnimAttack());
+         }
+         else anim.SetBool("Slash", false);
+         #endregion
+        
+        
         #region Jump
         if (Input.GetButtonDown("Jump")) anim.SetBool("Jump",true);
         else anim.SetBool("Jump", false);
         #endregion
+         
+        #region moving acceleration
+        
+        
+        maxVelocity=(Input.GetKey(KeyCode.LeftShift))? 1.0f : 0.5f; //check for sprint button & sets animation to sprint or walk
 
-        # region moving acceleration
-        if (Input.GetKey(KeyCode.LeftShift))
+        switch(Input.GetAxis("Vertical")) //check for Z movement
         {
-            maxVelocity = 1.0f;
-        }else
-        {
-            maxVelocity = 0.5f;
+            case 0: // decelerates when player stops
+                if ((velocityZ < 0)) velocityZ += deceleration * Time.deltaTime;
+                if ((velocityZ > 0)) velocityZ -= deceleration * Time.deltaTime;
+                break;
+            case float n when (n <0): // accelerates until maxVelocity reached
+                if (velocityZ > -maxVelocity) velocityZ -= acceleration * Time.deltaTime;
+                if (velocityZ < -maxVelocity) velocityZ += deceleration * Time.deltaTime;
+                break;
+            case float n when (n > 0):// idem
+                if (velocityZ < maxVelocity) velocityZ += acceleration * Time.deltaTime;
+                if (velocityZ > maxVelocity) velocityZ -= deceleration * Time.deltaTime;
+                break;
         }
 
-        if ((Input.GetAxis("Horizontal") != 0))
+        switch (Input.GetAxis("Horizontal")) // check for X movement
         {
-            if ((Input.GetAxis("Horizontal")>0) && velocityX < maxVelocity) velocityX +=  acceleration * Time.deltaTime;
-            if ((Input.GetAxis("Horizontal") < 0) && velocityX > -maxVelocity) velocityX += -1 * acceleration * Time.deltaTime;
-        }
-        if ((Input.GetAxis("Vertical") != 0))
-        {
-            if ((Input.GetAxis("Vertical") > 0) && (velocityZ < maxVelocity)) velocityZ +=   acceleration * Time.deltaTime;
-            if ((Input.GetAxis("Vertical") < 0) &&  (velocityZ > -maxVelocity)) velocityZ += -1 * acceleration * Time.deltaTime;
+            case 0:
+                if ((velocityX < 0)) velocityX += deceleration * Time.deltaTime;
+                if ((velocityX > 0)) velocityX -= deceleration * Time.deltaTime;
+                break;
+            case float n when (n < 0):
+
+                if(Input.GetAxis("Vertical")<0)
+                {
+                    if (velocityX < maxVelocity) velocityX += acceleration * Time.deltaTime;
+                    if (velocityX > maxVelocity) velocityX += -deceleration * Time.deltaTime;
+                }
+                else
+                {
+                    if (velocityX > -maxVelocity) velocityX += -acceleration * Time.deltaTime;
+                    if (velocityX < -maxVelocity) velocityX += deceleration * Time.deltaTime;
+                }
+               
+                break;
+
+            case float n when (n > 0):
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    if (velocityX > -maxVelocity) velocityX += -acceleration * Time.deltaTime;
+                    if (velocityX < -maxVelocity) velocityX += deceleration * Time.deltaTime;
+                }
+                else
+                {
+                    if (velocityX < maxVelocity) velocityX += acceleration * Time.deltaTime;
+                    if (velocityX > maxVelocity) velocityX += -deceleration * Time.deltaTime;
+                    
+                }
+                break;
         }
 
+       
 
-        if ((Input.GetAxis("Horizontal") == 0))
-        {
-            if (velocityX < 0) velocityX -= deceleration * Time.deltaTime;
-            if (velocityX > 0) velocityX += deceleration * Time.deltaTime;
-        }
-        if ((Input.GetAxis("Vertical") == 0))
-        {
-            if (velocityZ < 0) velocityZ -= deceleration*2 * Time.deltaTime;
-            if (velocityZ > 0) velocityZ += deceleration *2* Time.deltaTime;
-        }
+
         #endregion
 
-        
+
 
         anim.SetFloat("Velocity z ", velocityZ);
         anim.SetFloat("Velocity x", velocityX);
